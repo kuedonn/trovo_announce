5b1f209a5f0cf54cfbe43ef0259cd5a38ef11de4
DIONSTAM <58949888+kuedonn@users.noreply.github.com>
2022-06-15 12:04:22 +0300

    Update islive.js
    trying to make it dynamic

--- commands/islive.js
+++ commands/islive.js
@@ -5,30 +5,32 @@ const fetch = require('node-fetch');
 const dotenv  = require('dotenv');
 dotenv.config();
 
+const input = "tiesel1";
 
 module.exports = {
     data:new SlashCommandBuilder()
-    .setName('islive')
+    .setName('live')
     .setDescription('Trigger bot message on live'),
 async execute(interaction){
-fetch('https://open-api.trovo.live/openplatform/channels/id', {
+   fetch('https://open-api.trovo.live/openplatform/channels/id', {
     method: 'POST',
     headers: {
         'Accept': 'application/json',
         'Client-ID': process.env.CLIENT_ID,
         'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: '{"username":"tiesel1"}'
+    body: `{\"username\":\"${input}\"}`
 }).then(response=>response.json())
 .then(data=> {
+
 
     const file = new MessageAttachment('./assets/soon.png');
 
     if (data.is_live==true){
         const embed = new MessageEmbed()
-        .setAuthor({name: 'Tiesel is now live on Trovo!', url:'https://trovo.live/tiesel1'})
+        .setAuthor({name: 'Tiesel is now live on Trovo!', url:`https://trovo.live/${input}`})
         .setTitle((data.live_title).toString())
-        .setURL('https://trovo.live/tiesel')
+        .setURL(`https://trovo.live/${input}`)
         .setImage('attachment://soon.png')
         interaction.reply({embeds: [embed], files:[file]});
         }else{
