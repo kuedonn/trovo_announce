d6defaefaf848a3fe1c4bad96991d1fe85d335e5
DIONSTAM <58949888+kuedonn@users.noreply.github.com>
2022-06-19 11:09:04 +0300

    Update islive.js

--- commands/islive.js
+++ commands/islive.js
@@ -7,22 +7,22 @@ dotenv.config();
 
 
 module.exports = {
-    data:new SlashCommandBuilder()
+    data:new SlashCommandBuilder()  //create slash command
     .setName('live')
     .setDescription('Trigger bot message on live')
-    .addStringOption(option =>
+    .addStringOption(option =>        //create user input for the slash command
         option.setName('input')
         .setDescription('user input')
         .setRequired(true)
     ),
 async execute(interaction){
-    const input = interaction.options.getString('input');
-    const config = {
+    const input = interaction.options.getString('input');   // get user input
+    const config = {                                        //get the config with the options for the api request
         method: 'POST',
         url: 'https://open-api.trovo.live/openplatform/channels/id',
         headers: {
         'Accept': 'application/json',
-        'Client-ID': process.env.CLIENT_ID,
+        'Client-ID': process.env.CLIENT_ID,                     //you need to create a .env file with your client-id so its safe, you also need dotenv install
         'Content-Type': 'application/x-www-form-urlencoded'
         },
         data:`{\"username\":\"${input}\"}`
@@ -31,7 +31,7 @@ async execute(interaction){
 .then(response=> {
     const data = response.data;
 
-    if (data.is_live==true){
+    if (data.is_live==true){                  //if stream is live create the embed 
         const embed = new MessageEmbed()
         .setAuthor({name: `${input} is now live on Trovo!`, url:`https://trovo.live/${input}`})
         .setTitle((data.live_title).toString())
